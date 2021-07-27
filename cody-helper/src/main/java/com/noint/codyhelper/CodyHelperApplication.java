625279319976e03ef070cfe42fe071c52f11c6e9
N01NT <88023261+N01NT@users.noreply.github.com>
2021-07-27 16:09:32 +0900

    Add files via upload

---
+++ cody-helper/src/main/java/com/noint/codyhelper/CodyHelperApplication.java
@@ -0,0 +1,13 @@
+package com.noint.codyhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodyHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodyHelperApplication.class, args);
+    }
+
+}
---
+++ cody-helper/src/test/java/com/noint/codyhelper/CodyHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noint.codyhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodyHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
